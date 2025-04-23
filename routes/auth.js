de8ede7d28022ead4071b59fdac38f76ea3aa71d
SyukranDev <m.syukransoleh@amast.com.my>
2025-04-23 13:14:50 +0800

    implement login and register api

--- routes/auth.js
+++ routes/auth.js
@@ -2,32 +2,64 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-// Sign up route
 router.post('/signup', async (req, res) => {
     const { username, email, password } = req.body;
 
     try {
-        const user = await User.create({ username, email, password });
-        req.session.user = user;
+        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+        if (existingUser) {
+            return res.render('signup', {
+                pageTitle: 'Sign Up',
+                error: 'Username or email already exists'
+            });
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        req.session.user = { id: user._id, username: user.username };
         res.redirect('/');
     } catch (error) {
-        res.status(302).send(error.message);
+        res.render('signup', {
+            pageTitle: 'Sign Up',
+            error: error.message
+        });
     }
 });
 
-// Login route
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     try {
         const user = await User.findOne({ username });
-        if (!user || user.password !== password) {
-            return res.status(302).send('Invalid username or password');
+        if (!user) {
+            return res.render('login', {
+                pageTitle: 'Login',
+                error: 'Invalid username or password'
+            });
         }
-        req.session.user = user;
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.render('login', {
+                pageTitle: 'Login',
+                error: 'Invalid username or password'
+            });
+        }
+
+        req.session.user = { id: user._id, username: user.username };
         res.redirect('/');
     } catch (error) {
-        res.status(302).send(error.message);
+        res.render('login', {
+            pageTitle: 'Login',
+            error: error.message
+        });
     }
 });
 
